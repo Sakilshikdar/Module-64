11d30c481cf32311bbfad9f6b2b97b7831fb43ab
sakilshikdar <sakilshikdar2504@gmail.com>
2022-04-30 17:04:43 +0600

    first commit

--- index.js
+++ index.js
@@ -1,61 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient } = require("mongodb")
+require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 5000;
 
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello from my personal Smarty Pant!! with auto restart')
-});
 
-const users = [
-    { id: 1, name: 'Sabana', email: 'sabana@gmail.com', phone: '0178888888' },
-    { id: 2, name: 'Shabnoor', email: 'Shabnoor@gmail.com', phone: '0178888888' },
-    { id: 3, name: 'Suchorita', email: 'Suchorita@gmail.com', phone: '0178888888' },
-    { id: 4, name: 'suchonda', email: 'suchonda@gmail.com', phone: '0178888888' },
-    { id: 5, name: 'srabonti', email: 'srabonti@gmail.com', phone: '0178888888' },
-    { id: 6, name: 'sabila', email: 'sabila@gmail.com', phone: '0178888888' },
-    { id: 7, name: 'sohana', email: 'sohana@gmail.com', phone: '0178888888' },
-]
-
-app.get('/users', (req, res) => {
-    console.log('query', req.query);
-    if (req.query.name) {
-        const search = req.query.name.toLocaleLowerCase();
-        const match= users.filter(user=>user.name.toLocaleLowerCase().includes(search));
-        res.send(match)
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fsloq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+const run = async () => {
+    try {
+        await client.connect();
+        const productCollection = client.db('wearHouse2').collection('product')
+        app.get('/product', async(req, res)=>{
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
     }
-    else {
-        res.send(users);
+    catch (error) {
+        console.log(error);
     }
-})
-
-app.get('/user/:id', (req, res) => {
-    console.log(req.params);
-    const id = parseInt(req.params.id);
-    const user = users.find(u => u.id === id);
-    res.send(user);
-});
-
-
-app.post('/user', (req, res) => {
-    console.log('request', req.body);
-    const user = req.body;
-    user.id = users.length + 1;
-    users.push(user);
-    res.send(user);
-});
-
-app.get('/fruits', (req, res) => {
-    res.send(['mango', 'apple', 'oranges']);
-});
-
-app.get('/fruits/mango/fazle', (req, res) => {
-    res.send('sour sour fazle flavor');
-});
-
+}
+run()
 
 app.listen(port, () => {
     console.log('Listening to port', port)
